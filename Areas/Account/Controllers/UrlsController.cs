1139be4cfe29dfbdd49d9f3aa1a6c2cee1647b42
Boselor <xiaoyong@boselor.com>
2021-01-17 01:16:46 +0800

    增加前台访问

--- Areas/Account/Controllers/UrlsController.cs
+++ Areas/Account/Controllers/UrlsController.cs
@@ -14,7 +14,19 @@ namespace AireGo.Areas.Account.Controllers
         public UrlsController(IFreeSql sql) => _sql = sql;
         public IActionResult Index() => View();
         public IActionResult Add() => View();
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var ids = id ?? -1;
+            if (ids <= 0) return RedirectToAction("Index");
 
+            var res = await _sql.Select<UrlsMap>().Where(e => e.Id == id).FirstAsync();
+            if (res == null)
+                return Index();
+            else
+                ViewData["url_id"] = res.Id;
+
+            return View();
+        }
         public async Task<RespMap> Get(int page, int size)
         {
             var resp = new RespMap();
@@ -36,6 +48,101 @@ namespace AireGo.Areas.Account.Controllers
                 resp.Data = ex;
             }
 
+            return resp;
+        }
+        public async Task<RespMap> GetMap(int? id)
+        {
+            var ids = id ?? -1;
+            var resp = new RespMap();
+
+            try
+            {
+                var res = await _sql.Select<UrlsMap>().Where(e => e.Id == id).FirstAsync();
+                resp.Code = 200;
+                resp.Message = "获取成功";
+                resp.Data = res;
+            }
+            catch (Exception ex)
+            {
+                resp.Code = 500;
+                resp.Message = ex.Message;
+                resp.Data = ex;
+            }
+            return resp;
+        }
+        public async Task<RespMap> Push(int id, string title, string tag, string url,string posttime, string desc, string action = "add")
+        {
+            var resp = new RespMap();
+            try
+            {
+                var res = 0;
+
+                if (action.Equals("add"))
+                {
+                    var model = new UrlsMap()
+                    {
+                        Title = title,
+                        Posttime = DateTime.Now.ToString("yyyy/MM/dd"),
+                        Tag = tag,
+                        Url = url,
+                        Description = desc
+                    };
+                    res = await _sql
+                        .Insert(model)
+                        .ExecuteAffrowsAsync();
+
+                    if (res > 0)
+                    {
+                        resp.Code = 200;
+                        resp.Message = "添加成功";
+                        resp.Data = res;
+                    }
+                    else
+                    {
+                        resp.Code = 200;
+                        resp.Message = "添加失败";
+                        resp.Data = res;
+                    }
+                }
+                else if(action.Equals("edit"))
+                {
+                    var model = new UrlsMap()
+                    {
+                        Title = title,
+                        Posttime = posttime,
+                        Tag = tag,
+                        Url = url,
+                        Description = desc
+                    };
+
+                    res = await _sql
+                        .Update<UrlsMap>()
+                        .Where(e => e.Id == id)
+                        .Set(e => e.Title, model.Title)
+                        .Set(e => e.Url, model.Url)
+                        .Set(e => e.Description, model.Description)
+                        .ExecuteAffrowsAsync();
+
+                    if (res > 0)
+                    {
+                        resp.Code = 200;
+                        resp.Message = "更新成功";
+                        resp.Data = res;
+                    }
+                    else
+                    {
+                        resp.Code = 200;
+                        resp.Message = "更新失败";
+                        resp.Data = res;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resp.Code = 500;
+                resp.Message = ex.Message;
+                resp.Data = ex;
+            }
             return resp;
         }
     }
---
+++ Controllers/FwLinkController.cs
@@ -0,0 +1,27 @@
+﻿using AireGo.Areas.Account.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AireGo.Controllers
+{
+    public class FwLinkController : Controller
+    {
+        private IFreeSql _sql;
+        public FwLinkController(IFreeSql sql) => _sql = sql;
+        public async Task<IActionResult> Index(int? id)
+        {
+            var map = await _sql.Select<UrlsMap>().Where(e => e.Id == id).FirstAsync();
+            if (map == null)
+                return RedirectToAction("Error");
+            else
+                return Redirect(map.Url);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
